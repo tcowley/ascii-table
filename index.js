fa43769b97c54828f148192d4377cb748898d2e5
Trevor Cowley <tcowley@gmail.com>
2015-09-13 08:44:48 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+var table = module.exports = require('./lib/table');
+
---
+++ test/lib/align.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+var table = require('../../lib/table.js');
+
+//var str = 'one two three four five six seven eight ninetynine onhundred';
+var str = 'one two three four'; 
+[
+    [str, 30, 'left'],
+    [str, 30, 'right'],
+    [str, 31, 'center'],
+    [str, 30, 'justify'],
+].forEach(function(line) {
+    var output = table.fillRowLine.apply(null, line);
+    console.log(output, '|', line[2]);
+});
+
+
